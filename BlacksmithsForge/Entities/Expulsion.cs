5b90ae14fcaeca28bda86e86ba04f3925c88cd10
justastranger <justastrangerlica@gmail.com>
2022-12-18 14:25:18 -0600

    Add Expulsion and RecipeLink for support for more root entity properties

---
+++ BlacksmithsForge/Entities/Expulsion.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlacksmithsForge.Entities
+{
+    internal class Expulsion : IEntity
+    {
+        public JObject EntityData { get; set; }
+        // For this Entity, the ID property determines the target
+        // It should never be null
+        public string ID
+        {
+            get
+            {
+                if (EntityData["id"] == null) throw new NullReferenceException("RecipeLink ID must be specified.");
+                else return EntityData["id"].ToString();
+            }
+            set => EntityData["id"] = value;
+        }
+        public Guid Guid { get; set; } = new();
+        public Dictionary<string, string>? Filter { get { return EntityData["filter"]?.ToObject<Dictionary<string, string>>(); } set => EntityData["filter"] = value != null ? JObject.FromObject(value) : null; }
+        public int? Limit { get { return EntityData["limit"]?.ToObject<int>(); } set => EntityData["limit"] = value; }
+        public string? ToPath { get { return EntityData["topath"]?.ToString(); } set => EntityData["topath"] = value; }
+
+        public Expulsion(JObject entityData)
+        {
+            EntityData = entityData;
+        }
+
+    }
+}
---
+++ BlacksmithsForge/Entities/RecipeLink.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlacksmithsForge.Entities
+{
+    internal class RecipeLink : IEntity
+    {
+
+
+        public JObject EntityData { get; set; }
+        // For this Entity, the ID property determines the target for the link
+        // It should never be null
+        public string ID
+        {
+            get
+            {
+                if (EntityData["id"] == null) throw new NullReferenceException("RecipeLink ID must be specified.");
+                else return EntityData["id"].ToString();
+            }
+            set => EntityData["id"] = value;
+        }
+        public Guid Guid { get; set; } = new();
+
+        public int? Chance { get { return EntityData["chance"]?.ToObject<int>(); } set => EntityData["chance"] = value; }
+        public bool? Additional { get { return EntityData["additional"]?.ToObject<bool>(); } set => EntityData["additional"] = value; }
+        public string? ToPath { get { return EntityData["topath"]?.ToString(); } set => EntityData["topath"] = value; }
+        public Dictionary<string, string>? Challenges { get { return EntityData["challenges"]?.ToObject<Dictionary<string, string>>(); } set => EntityData["challenges"] = value != null ? JObject.FromObject(value) : null; }
+        public Expulsion? Expulsion { get { return EntityData["challenges"]?.ToObject<Expulsion>(); } set => EntityData["challenges"] = value != null ? JObject.FromObject(value) : null; }
+        
+
+        public RecipeLink(JObject entityData)
+        {
+            EntityData = entityData;
+        }
+    }
+}
