3717904cd2e1aef9d74a5640e38adc588de40482
justastranger <justastrangerlica@gmail.com>
2022-12-18 15:04:17 -0600

    Add XTrigger

---
+++ BlacksmithsForge/Entities/XTrigger.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlacksmithsForge.Entities
+{
+    internal class XTrigger : IEntity
+    {
+        public JObject EntityData { get; set; }
+        // determines result
+        public string ID
+        {
+            get
+            {
+                if (EntityData["id"] == null) throw new NullReferenceException("XTrigger ID must be specified.");
+                else return EntityData["id"].ToString();
+            }
+            set => EntityData["id"] = value;
+        }
+        public Guid Guid { get; set; } = new();
+
+        public int? Chance { get { return EntityData["chance"]?.ToObject<int>(); } set => EntityData["chance"] = value; }
+        public int? Level { get { return EntityData["level"]?.ToObject<int>(); } set => EntityData["level"] = value; }
+        public string? MorphEffect { get { return EntityData["morpheffect"]?.ToString(); } set => EntityData["morpheffect"] = value; }
+
+
+
+
+        public XTrigger(JObject entityData)
+        {
+            EntityData = entityData;
+        }
+
+        // anticipating quickspec support
+        public XTrigger(string id)
+        {
+            EntityData = new();
+            ID = id;
+            Chance = 100;
+            Level = 1;
+            MorphEffect = "Transform";
+        }
+    }
+}
