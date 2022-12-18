6fce268fd06d490fc46bb435815147a21be55af7
justastranger <justastrangerlica@gmail.com>
2022-12-17 23:02:07 -0600

    For future reference Should expand with child entities

---
+++ BlacksmithsForge/Entities/EntityTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlacksmithsForge.Entities
+{
+    internal enum EntityTypes
+    {
+        ELEMENT,
+        VERB,
+        RECIPE,
+        PORTAL,
+        DECK,
+        LEGACY,
+        LEVER,
+        ENDING,
+        ACHIEVEMENT,
+        CULTURE,
+        SETTING
+
+    }
+}
