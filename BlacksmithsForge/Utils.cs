a3bfb62a0009fb6bc66e4d8c2594d6d73baace07
justastranger <justastrangerlica@gmail.com>
2022-12-18 00:00:46 -0600

    Currently only used for EntityFactory to swap the plural entity type names encountered in the JSON for singular versions of the words

---
+++ BlacksmithsForge/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlacksmithsForge
+{
+    internal class Utils
+    {
+        public static string PluralToSingular(string name) {
+            switch (name)
+            {
+                case "achievements": return "achievement";
+                case "cultures": return "culture";
+                case "decks": return "deck";
+                case "elements": return "element";
+                case "endings": return "ending";
+                case "legacies": return "legacy";
+                case "levers": return "lever";
+                case "portals": return "portal";
+                case "recipes": return "recipe";
+                case "settings": return "setting";
+                case "verbs": return "verb";
+                default: return "unknown";
+            }
+        }
+    }
+}
