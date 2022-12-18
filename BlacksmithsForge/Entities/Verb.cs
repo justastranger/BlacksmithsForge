8b9764eae25ca70cc10fdb3b960d4808797470ad
justastranger <justastrangerlica@gmail.com>
2022-12-18 16:56:52 -0600

    Add properties to Verb that was accidentally included in the previous commit unfinished

--- BlacksmithsForge/Entities/Verb.cs
+++ BlacksmithsForge/Entities/Verb.cs
@@ -22,6 +22,19 @@ namespace BlacksmithsForge.Entities
         public Guid Guid { get; set; } = new();
         public string? Filename { get; set; }
 
+        public string? Label { get { return EntityData["label"]?.ToString(); } set => EntityData["label"] = value; }
+        public string? Description { get { return EntityData["description"]?.ToString(); } set => EntityData["description"] = value; }
+        public string? Icon { get { return EntityData["icon"]?.ToString(); } set => EntityData["icon"] = value; }
+        public bool? Directable { get { return EntityData["directable"]?.ToObject<bool>(); } set => EntityData["directable"] = value; }
+        public string? Category { get { return EntityData["category"]?.ToString(); } set => EntityData["category"] = value; }
+        public bool? Spontaneous { get { return EntityData["spontaneous"]?.ToObject<bool>(); } set => EntityData["spontaneous"] = value; }
+        public Sphere? Slot { get { return EntityData["slot"]?.ToObject<Sphere>(); } set => EntityData["slot"] = value != null ? JObject.FromObject(value) : null; }
+        public List<Sphere>? Slots { get { return (List<Sphere>?)(EntityData["slots"]?.Values<Sphere>()); } set => EntityData["slots"] = value != null ? JArray.FromObject(value) : null; }
+        public Dictionary<string, int>? Aspects { get { return EntityData["aspects"]?.ToObject<Dictionary<string, int>>(); } set => EntityData["aspects"] = value != null ? JObject.FromObject(value) : null; }
+        public Dictionary<string, List<XTrigger>>? XTriggers { get { return EntityData["induces"]?.ToObject<Dictionary<string, List<XTrigger>>>(); } set => EntityData["induces"] = value != null ? JObject.FromObject(value) : null; }
+
+
+
         public Verb(JObject entityData)
         {
             EntityData = entityData;
