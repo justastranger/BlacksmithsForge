a9e6995b97a3cb8ef927ed1c382f950828052cdc
justastranger <justastrangerlica@gmail.com>
2022-12-19 02:13:17 -0600

    Add JSON editor

---
+++ BlacksmithsForge/Editors/JsonEditor.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace BlacksmithsForge.Editors
+{
+    partial class JsonEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scintilla1 = new ScintillaNET.Scintilla();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // scintilla1
+            // 
+            this.scintilla1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scintilla1.AutoCMaxHeight = 9;
+            this.scintilla1.BiDirectionality = ScintillaNET.BiDirectionalDisplayType.Disabled;
+            this.scintilla1.CaretLineBackColor = System.Drawing.Color.White;
+            this.scintilla1.CaretLineVisible = true;
+            this.scintilla1.Lexer = ScintillaNET.Lexer.Json;
+            this.scintilla1.LexerName = "json";
+            this.scintilla1.Location = new System.Drawing.Point(12, 12);
+            this.scintilla1.Name = "scintilla1";
+            this.scintilla1.ScrollWidth = 49;
+            this.scintilla1.Size = new System.Drawing.Size(645, 397);
+            this.scintilla1.TabIndents = true;
+            this.scintilla1.TabIndex = 0;
+            this.scintilla1.UseRightToLeftReadingLayout = false;
+            this.scintilla1.WrapMode = ScintillaNET.WrapMode.None;
+            this.scintilla1.TextChanged += new System.EventHandler(this.scintilla1_TextChanged);
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.acceptButton.Location = new System.Drawing.Point(12, 415);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(75, 23);
+            this.acceptButton.TabIndex = 1;
+            this.acceptButton.Text = "Accept";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(582, 415);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // JsonEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 450);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.scintilla1);
+            this.Name = "JsonEditor";
+            this.Text = "JsonEditor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScintillaNET.Scintilla scintilla1;
+        private Button acceptButton;
+        private Button cancelButton;
+    }
+}
---
+++ BlacksmithsForge/Editors/JsonEditor.cs
@@ -0,0 +1,60 @@
+﻿using ScintillaNET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlacksmithsForge.Editors
+{
+    public partial class JsonEditor : Form
+    {
+        public string jsonText;
+        string tmpText;
+
+        public JsonEditor(string text)
+        {
+            InitializeComponent();
+            InitializeScintilla();
+            jsonText = text;
+            scintilla1.Text = jsonText;
+        }
+
+        private void InitializeScintilla()
+        {
+            scintilla1.Styles[Style.Json.Default].ForeColor = Color.Silver;
+            scintilla1.Styles[Style.Json.BlockComment].ForeColor = Color.Green; // Green
+            scintilla1.Styles[Style.Json.LineComment].ForeColor = Color.Green; // Green
+            scintilla1.Styles[Style.Json.Number].ForeColor = Color.Olive;
+            scintilla1.Styles[Style.Json.PropertyName].ForeColor = Color.Blue;
+            scintilla1.Styles[Style.Json.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla1.Styles[Style.Json.StringEol].BackColor = Color.Pink;
+            scintilla1.Styles[Style.Json.Operator].ForeColor = Color.Purple;
+            scintilla1.Styles[Style.Json.Keyword].ForeColor = Color.OrangeRed;
+
+            scintilla1.SetKeywords(0, "true false");
+        }
+
+        private void scintilla1_TextChanged(object sender, EventArgs e)
+        {
+            tmpText = scintilla1.Text;
+        }
+
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+            jsonText = tmpText;
+            DialogResult = DialogResult.Yes;
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
