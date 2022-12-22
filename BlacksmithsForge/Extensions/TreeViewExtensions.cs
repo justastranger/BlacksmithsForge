84811cce9a6c5630a4fafeb3dcedae480884f49f
justastranger <justastrangerlica@gmail.com>
2022-12-21 21:30:34 -0600

    Add TreeView extension for loading JSON Just need to figure out a way to make modifications to the TreeView's data, push those changes into the underlying JObject, and then update the original copy (if accepted)

---
+++ BlacksmithsForge/Extensions/TreeViewExtensions.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlacksmithsForge.Extensions
+{
+    // Extension found here
+    // Licensed under MIT
+    // https://github.com/huseyint/JsonTreeView/blob/master/JsonTreeView/JsonTreeViewLoader.cs
+
+    internal static class TreeViewExtensions
+    {
+        public static void LoadJsonToTreeView(this TreeView treeView, string json)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return;
+            }
+
+            var @object = JObject.Parse(json);
+            AddObjectNodes(@object, "JSON", treeView.Nodes);
+        }
+
+        public static void AddObjectNodes(JObject @object, string name, TreeNodeCollection parent)
+        {
+            var node = new TreeNode(name);
+            parent.Add(node);
+
+            foreach (var property in @object.Properties())
+            {
+                AddTokenNodes(property.Value, property.Name, node.Nodes);
+            }
+        }
+
+        private static void AddArrayNodes(JArray array, string name, TreeNodeCollection parent)
+        {
+            var node = new TreeNode(name);
+            parent.Add(node);
+
+            for (var i = 0; i < array.Count; i++)
+            {
+                AddTokenNodes(array[i], string.Format("[{0}]", i), node.Nodes);
+            }
+        }
+
+        private static void AddTokenNodes(JToken token, string name, TreeNodeCollection parent)
+        {
+            if (token is JValue value)
+            {
+                parent.Add(new TreeNode(string.Format("{0}: {1}", name, value.Value)));
+            }
+            else if (token is JArray array)
+            {
+                AddArrayNodes(array, name, parent);
+            }
+            else if (token is JObject @object)
+            {
+                AddObjectNodes(@object, name, parent);
+            }
+        }
+    }
+}
