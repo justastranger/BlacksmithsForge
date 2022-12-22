4e85ca9b017b5a77f555450c71fabdf9c8503400
justastranger <justastrangerlica@gmail.com>
2022-12-21 21:32:57 -0600

    Start making the TreeView form Should be able to display JSON right out of the box, needs testing

---
+++ BlacksmithsForge/Editors/JsonTreeViewEditor.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace BlacksmithsForge.Editors
+{
+    partial class JsonTreeViewEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.jsonTreeView = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // jsonTreeView
+            // 
+            this.jsonTreeView.Location = new System.Drawing.Point(12, 12);
+            this.jsonTreeView.Name = "jsonTreeView";
+            this.jsonTreeView.Size = new System.Drawing.Size(707, 426);
+            this.jsonTreeView.TabIndex = 0;
+            // 
+            // JsonTreeViewEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 450);
+            this.Controls.Add(this.jsonTreeView);
+            this.Name = "JsonTreeViewEditor";
+            this.Text = "JsonTreeViewEditor";
+            this.Load += new System.EventHandler(this.JsonTreeViewEditor_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TreeView jsonTreeView;
+    }
+}
---
+++ BlacksmithsForge/Editors/JsonTreeViewEditor.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BlacksmithsForge.Extensions;
+
+namespace BlacksmithsForge.Editors
+{
+    public partial class JsonTreeViewEditor : Form
+    {
+        readonly JObject currentEntity;
+
+        public JsonTreeViewEditor(JObject entityToEdit)
+        {
+            currentEntity = entityToEdit ?? throw new NullReferenceException("Can't edit a null JObject");
+            InitializeComponent();
+        }
+
+        private void JsonTreeViewEditor_Load(object sender, EventArgs e)
+        {
+            string json = currentEntity.ToString();
+            jsonTreeView.LoadJsonToTreeView(json);
+        }
+    }
+}
