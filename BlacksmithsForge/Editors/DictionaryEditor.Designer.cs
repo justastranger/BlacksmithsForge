5877a382a4186495fa61819790993bead633ef3c
justastranger <justastrangerlica@gmail.com>
2022-12-25 14:21:00 -0600

    Add a Dictionary editor Made only for dictionaries with strings as keys, value can be any type

---
+++ BlacksmithsForge/Editors/DictionaryEditor.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BlacksmithsForge.Editors
+{
+    partial class DictionaryEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dictionaryDataGridView = new System.Windows.Forms.DataGridView();
+            this.okayButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dictionaryDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dictionaryDataGridView
+            // 
+            this.dictionaryDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dictionaryDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dictionaryDataGridView.Name = "dictionaryDataGridView";
+            this.dictionaryDataGridView.RowTemplate.Height = 25;
+            this.dictionaryDataGridView.Size = new System.Drawing.Size(602, 397);
+            this.dictionaryDataGridView.TabIndex = 0;
+            this.dictionaryDataGridView.RowValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dictionaryDataGridView_RowValidated);
+            this.dictionaryDataGridView.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dictionaryDataGridView_UserDeletedRow);
+            // 
+            // okayButton
+            // 
+            this.okayButton.Location = new System.Drawing.Point(276, 415);
+            this.okayButton.Name = "okayButton";
+            this.okayButton.Size = new System.Drawing.Size(75, 23);
+            this.okayButton.TabIndex = 1;
+            this.okayButton.Text = "Okay";
+            this.okayButton.UseVisualStyleBackColor = true;
+            this.okayButton.Click += new System.EventHandler(this.okayButton_Click);
+            // 
+            // DictionaryEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 450);
+            this.Controls.Add(this.okayButton);
+            this.Controls.Add(this.dictionaryDataGridView);
+            this.Name = "DictionaryEditor";
+            this.Text = "DictionaryEditor";
+            ((System.ComponentModel.ISupportInitialize)(this.dictionaryDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dictionaryDataGridView;
+        private Button okayButton;
+    }
+}
---
+++ BlacksmithsForge/Editors/DictionaryEditor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlacksmithsForge.Editors
+{
+    public partial class DictionaryEditor : Form
+    {
+        public Dictionary<string, dynamic> Dictionary;
+        private Type valueType;
+
+        public DictionaryEditor(Dictionary<string, dynamic> dictionaryToEdit)
+        {
+            InitializeComponent();
+            Dictionary = dictionaryToEdit;
+            valueType = dictionaryToEdit.GetType().GetGenericArguments()[1];
+            UpdateDataGridView();
+        }
+
+        private void okayButton_Click(object sender, EventArgs e)
+        {
+            UpdateDictionary();
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void UpdateDataGridView()
+        {
+            foreach (var pair in Dictionary)
+            {
+                dictionaryDataGridView.Rows.Add(pair.Key, pair.Value);
+            }
+        }
+
+        private void UpdateDictionary()
+        {
+            Dictionary.Clear();
+
+            foreach (DataGridViewRow row in dictionaryDataGridView.Rows)
+            {
+                if (row.Cells[0].Value?.ToString() != null && row.Cells[1].Value?.ToString() != null)
+                {
+                    // null checked above, idk why it is so damned sure there's a null reference possibility
+                    
+                    string key = row.Cells[0].Value.ToString();
+                    dynamic value;
+                    if (valueType == typeof(string))
+                    {
+                        value = row.Cells[1].Value.ToString();
+                    }
+                    else if (valueType == typeof(int))
+                    {
+                        value = int.Parse(row.Cells[1].Value.ToString());
+                    } else
+                    {
+                        value = row.Cells[1].Value;
+                    }
+                    
+
+                    Dictionary.Add(key, value);
+                }
+            }
+        }
+
+        private void dictionaryDataGridView_UserDeletedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            UpdateDictionary();
+        }
+
+        private void dictionaryDataGridView_RowValidated(object sender, DataGridViewCellEventArgs e)
+        {
+            if (string.IsNullOrEmpty(dictionaryDataGridView.Rows[e.RowIndex].Cells[0].Value?.ToString()) &&
+                string.IsNullOrEmpty(dictionaryDataGridView.Rows[e.RowIndex].Cells[1].Value?.ToString()))
+            {
+                UpdateDictionary();
+            }
+        }
+    }
+}
