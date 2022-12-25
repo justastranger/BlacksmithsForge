a7ee747d4aa8c47dc91ebc04f9d251ca59feb6f0
justastranger <justastrangerlica@gmail.com>
2022-12-25 16:45:58 -0600

    Make DictionaryEditor appear in the center of the screen

--- BlacksmithsForge/Editors/DictionaryEditor.Designer.cs
+++ BlacksmithsForge/Editors/DictionaryEditor.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.dictionaryDataGridView = new System.Windows.Forms.DataGridView();
-            this.okayButton = new System.Windows.Forms.Button();
             this.keyColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.valueColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.okayButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dictionaryDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,16 +52,6 @@
             this.dictionaryDataGridView.RowValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dictionaryDataGridView_RowValidated);
             this.dictionaryDataGridView.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dictionaryDataGridView_UserDeletedRow);
             // 
-            // okayButton
-            // 
-            this.okayButton.Location = new System.Drawing.Point(276, 415);
-            this.okayButton.Name = "okayButton";
-            this.okayButton.Size = new System.Drawing.Size(75, 23);
-            this.okayButton.TabIndex = 1;
-            this.okayButton.Text = "Okay";
-            this.okayButton.UseVisualStyleBackColor = true;
-            this.okayButton.Click += new System.EventHandler(this.okayButton_Click);
-            // 
             // keyColumn
             // 
             this.keyColumn.HeaderText = "Key";
@@ -72,6 +62,16 @@
             this.valueColumn.HeaderText = "Value";
             this.valueColumn.Name = "valueColumn";
             // 
+            // okayButton
+            // 
+            this.okayButton.Location = new System.Drawing.Point(276, 415);
+            this.okayButton.Name = "okayButton";
+            this.okayButton.Size = new System.Drawing.Size(75, 23);
+            this.okayButton.TabIndex = 1;
+            this.okayButton.Text = "Okay";
+            this.okayButton.UseVisualStyleBackColor = true;
+            this.okayButton.Click += new System.EventHandler(this.okayButton_Click);
+            // 
             // DictionaryEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -80,6 +80,7 @@
             this.Controls.Add(this.okayButton);
             this.Controls.Add(this.dictionaryDataGridView);
             this.Name = "DictionaryEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DictionaryEditor";
             ((System.ComponentModel.ISupportInitialize)(this.dictionaryDataGridView)).EndInit();
             this.ResumeLayout(false);
