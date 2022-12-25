a13b5969fe1c42f6536f49e213246556ca409009
justastranger <justastrangerlica@gmail.com>
2022-12-25 15:47:47 -0600

    Add Sphere Editor

---
+++ BlacksmithsForge/Editors/SphereInput.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace BlacksmithsForge.Editors
+{
+    partial class SphereInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idLabel = new System.Windows.Forms.Label();
+            this.labelLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.actionIdLabel = new System.Windows.Forms.Label();
+            this.fromPathLabel = new System.Windows.Forms.Label();
+            this.enRouteSpherePathLabel = new System.Windows.Forms.Label();
+            this.windowsSpherePathLabel = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.labelTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.actionIdTextBox = new System.Windows.Forms.TextBox();
+            this.fromPathTextBox = new System.Windows.Forms.TextBox();
+            this.enRouteSpherePathTextBox = new System.Windows.Forms.TextBox();
+            this.windowsSpherePathTextBox = new System.Windows.Forms.TextBox();
+            this.consumesCheckBox = new System.Windows.Forms.CheckBox();
+            this.greedyCheckBox = new System.Windows.Forms.CheckBox();
+            this.essentialButton = new System.Windows.Forms.Button();
+            this.requiredButton = new System.Windows.Forms.Button();
+            this.forbiddenButton = new System.Windows.Forms.Button();
+            this.ifAspectsPresentButton = new System.Windows.Forms.Button();
+            this.okayButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(12, 15);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(18, 15);
+            this.idLabel.TabIndex = 0;
+            this.idLabel.Text = "ID";
+            // 
+            // labelLabel
+            // 
+            this.labelLabel.AutoSize = true;
+            this.labelLabel.Location = new System.Drawing.Point(12, 44);
+            this.labelLabel.Name = "labelLabel";
+            this.labelLabel.Size = new System.Drawing.Size(35, 15);
+            this.labelLabel.TabIndex = 1;
+            this.labelLabel.Text = "Label";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(12, 73);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(67, 15);
+            this.descriptionLabel.TabIndex = 2;
+            this.descriptionLabel.Text = "Description";
+            // 
+            // actionIdLabel
+            // 
+            this.actionIdLabel.AutoSize = true;
+            this.actionIdLabel.Location = new System.Drawing.Point(12, 102);
+            this.actionIdLabel.Name = "actionIdLabel";
+            this.actionIdLabel.Size = new System.Drawing.Size(44, 15);
+            this.actionIdLabel.TabIndex = 3;
+            this.actionIdLabel.Text = "Verb ID";
+            // 
+            // fromPathLabel
+            // 
+            this.fromPathLabel.AutoSize = true;
+            this.fromPathLabel.Location = new System.Drawing.Point(12, 131);
+            this.fromPathLabel.Name = "fromPathLabel";
+            this.fromPathLabel.Size = new System.Drawing.Size(62, 15);
+            this.fromPathLabel.TabIndex = 4;
+            this.fromPathLabel.Text = "From Path";
+            // 
+            // enRouteSpherePathLabel
+            // 
+            this.enRouteSpherePathLabel.AutoSize = true;
+            this.enRouteSpherePathLabel.Location = new System.Drawing.Point(12, 160);
+            this.enRouteSpherePathLabel.Name = "enRouteSpherePathLabel";
+            this.enRouteSpherePathLabel.Size = new System.Drawing.Size(120, 15);
+            this.enRouteSpherePathLabel.TabIndex = 5;
+            this.enRouteSpherePathLabel.Text = "En Route Sphere Path";
+            // 
+            // windowsSpherePathLabel
+            // 
+            this.windowsSpherePathLabel.AutoSize = true;
+            this.windowsSpherePathLabel.Location = new System.Drawing.Point(12, 189);
+            this.windowsSpherePathLabel.Name = "windowsSpherePathLabel";
+            this.windowsSpherePathLabel.Size = new System.Drawing.Size(122, 15);
+            this.windowsSpherePathLabel.TabIndex = 6;
+            this.windowsSpherePathLabel.Text = "Windows Sphere Path";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(36, 12);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(245, 23);
+            this.idTextBox.TabIndex = 7;
+            this.idTextBox.TextChanged += new System.EventHandler(this.idTextBox_TextChanged);
+            // 
+            // labelTextBox
+            // 
+            this.labelTextBox.Location = new System.Drawing.Point(53, 41);
+            this.labelTextBox.Name = "labelTextBox";
+            this.labelTextBox.Size = new System.Drawing.Size(228, 23);
+            this.labelTextBox.TabIndex = 8;
+            this.labelTextBox.TextChanged += new System.EventHandler(this.labelTextBox_TextChanged);
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(85, 70);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(196, 23);
+            this.descriptionTextBox.TabIndex = 9;
+            this.descriptionTextBox.TextChanged += new System.EventHandler(this.descriptionTextBox_TextChanged);
+            // 
+            // actionIdTextBox
+            // 
+            this.actionIdTextBox.Location = new System.Drawing.Point(62, 99);
+            this.actionIdTextBox.Name = "actionIdTextBox";
+            this.actionIdTextBox.Size = new System.Drawing.Size(219, 23);
+            this.actionIdTextBox.TabIndex = 10;
+            this.actionIdTextBox.TextChanged += new System.EventHandler(this.actionIdTextBox_TextChanged);
+            // 
+            // fromPathTextBox
+            // 
+            this.fromPathTextBox.Location = new System.Drawing.Point(80, 128);
+            this.fromPathTextBox.Name = "fromPathTextBox";
+            this.fromPathTextBox.Size = new System.Drawing.Size(201, 23);
+            this.fromPathTextBox.TabIndex = 11;
+            this.fromPathTextBox.TextChanged += new System.EventHandler(this.fromPathTextBox_TextChanged);
+            // 
+            // enRouteSpherePathTextBox
+            // 
+            this.enRouteSpherePathTextBox.Location = new System.Drawing.Point(138, 157);
+            this.enRouteSpherePathTextBox.Name = "enRouteSpherePathTextBox";
+            this.enRouteSpherePathTextBox.Size = new System.Drawing.Size(143, 23);
+            this.enRouteSpherePathTextBox.TabIndex = 12;
+            this.enRouteSpherePathTextBox.TextChanged += new System.EventHandler(this.enRouteSpherePathTextBox_TextChanged);
+            // 
+            // windowsSpherePathTextBox
+            // 
+            this.windowsSpherePathTextBox.Location = new System.Drawing.Point(140, 186);
+            this.windowsSpherePathTextBox.Name = "windowsSpherePathTextBox";
+            this.windowsSpherePathTextBox.Size = new System.Drawing.Size(141, 23);
+            this.windowsSpherePathTextBox.TabIndex = 13;
+            this.windowsSpherePathTextBox.TextChanged += new System.EventHandler(this.windowsSpherePathTextBox_TextChanged);
+            // 
+            // consumesCheckBox
+            // 
+            this.consumesCheckBox.AutoSize = true;
+            this.consumesCheckBox.Location = new System.Drawing.Point(140, 215);
+            this.consumesCheckBox.Name = "consumesCheckBox";
+            this.consumesCheckBox.Size = new System.Drawing.Size(82, 19);
+            this.consumesCheckBox.TabIndex = 14;
+            this.consumesCheckBox.Text = "Consumes";
+            this.consumesCheckBox.UseVisualStyleBackColor = true;
+            this.consumesCheckBox.CheckedChanged += new System.EventHandler(this.consumesCheckBox_CheckedChanged);
+            // 
+            // greedyCheckBox
+            // 
+            this.greedyCheckBox.AutoSize = true;
+            this.greedyCheckBox.Location = new System.Drawing.Point(53, 215);
+            this.greedyCheckBox.Name = "greedyCheckBox";
+            this.greedyCheckBox.Size = new System.Drawing.Size(63, 19);
+            this.greedyCheckBox.TabIndex = 15;
+            this.greedyCheckBox.Text = "Greedy";
+            this.greedyCheckBox.UseVisualStyleBackColor = true;
+            this.greedyCheckBox.CheckedChanged += new System.EventHandler(this.greedyCheckBox_CheckedChanged);
+            // 
+            // essentialButton
+            // 
+            this.essentialButton.Location = new System.Drawing.Point(287, 12);
+            this.essentialButton.Name = "essentialButton";
+            this.essentialButton.Size = new System.Drawing.Size(175, 23);
+            this.essentialButton.TabIndex = 16;
+            this.essentialButton.Text = "Edit Essential Elements";
+            this.essentialButton.UseVisualStyleBackColor = true;
+            this.essentialButton.Click += new System.EventHandler(this.essentialButton_Click);
+            // 
+            // requiredButton
+            // 
+            this.requiredButton.Location = new System.Drawing.Point(287, 41);
+            this.requiredButton.Name = "requiredButton";
+            this.requiredButton.Size = new System.Drawing.Size(175, 23);
+            this.requiredButton.TabIndex = 17;
+            this.requiredButton.Text = "Edit Required Elements";
+            this.requiredButton.UseVisualStyleBackColor = true;
+            this.requiredButton.Click += new System.EventHandler(this.requiredButton_Click);
+            // 
+            // forbiddenButton
+            // 
+            this.forbiddenButton.Location = new System.Drawing.Point(287, 70);
+            this.forbiddenButton.Name = "forbiddenButton";
+            this.forbiddenButton.Size = new System.Drawing.Size(175, 23);
+            this.forbiddenButton.TabIndex = 18;
+            this.forbiddenButton.Text = "Edit Forbidden Elements";
+            this.forbiddenButton.UseVisualStyleBackColor = true;
+            this.forbiddenButton.Click += new System.EventHandler(this.forbiddenButton_Click);
+            // 
+            // ifAspectsPresentButton
+            // 
+            this.ifAspectsPresentButton.Location = new System.Drawing.Point(287, 98);
+            this.ifAspectsPresentButton.Name = "ifAspectsPresentButton";
+            this.ifAspectsPresentButton.Size = new System.Drawing.Size(175, 23);
+            this.ifAspectsPresentButton.TabIndex = 19;
+            this.ifAspectsPresentButton.Text = "Edit Aspect Dependencies";
+            this.ifAspectsPresentButton.UseVisualStyleBackColor = true;
+            this.ifAspectsPresentButton.Click += new System.EventHandler(this.ifAspectsPresentButton_Click);
+            // 
+            // okayButton
+            // 
+            this.okayButton.Location = new System.Drawing.Point(228, 215);
+            this.okayButton.Name = "okayButton";
+            this.okayButton.Size = new System.Drawing.Size(75, 23);
+            this.okayButton.TabIndex = 20;
+            this.okayButton.Text = "Okay";
+            this.okayButton.UseVisualStyleBackColor = true;
+            this.okayButton.Click += new System.EventHandler(this.okayButton_Click);
+            // 
+            // SphereInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 250);
+            this.Controls.Add(this.okayButton);
+            this.Controls.Add(this.ifAspectsPresentButton);
+            this.Controls.Add(this.forbiddenButton);
+            this.Controls.Add(this.requiredButton);
+            this.Controls.Add(this.essentialButton);
+            this.Controls.Add(this.greedyCheckBox);
+            this.Controls.Add(this.consumesCheckBox);
+            this.Controls.Add(this.windowsSpherePathTextBox);
+            this.Controls.Add(this.enRouteSpherePathTextBox);
+            this.Controls.Add(this.fromPathTextBox);
+            this.Controls.Add(this.actionIdTextBox);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.labelTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.windowsSpherePathLabel);
+            this.Controls.Add(this.enRouteSpherePathLabel);
+            this.Controls.Add(this.fromPathLabel);
+            this.Controls.Add(this.actionIdLabel);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.labelLabel);
+            this.Controls.Add(this.idLabel);
+            this.Name = "SphereInput";
+            this.Text = "SlotInput";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label idLabel;
+        private Label labelLabel;
+        private Label descriptionLabel;
+        private Label actionIdLabel;
+        private Label fromPathLabel;
+        private Label enRouteSpherePathLabel;
+        private Label windowsSpherePathLabel;
+        private TextBox idTextBox;
+        private TextBox labelTextBox;
+        private TextBox descriptionTextBox;
+        private TextBox actionIdTextBox;
+        private TextBox fromPathTextBox;
+        private TextBox enRouteSpherePathTextBox;
+        private TextBox windowsSpherePathTextBox;
+        private CheckBox consumesCheckBox;
+        private CheckBox greedyCheckBox;
+        private Button essentialButton;
+        private Button requiredButton;
+        private Button forbiddenButton;
+        private Button ifAspectsPresentButton;
+        private Button okayButton;
+    }
+}
---
+++ BlacksmithsForge/Editors/SphereInput.cs
@@ -0,0 +1,127 @@
+﻿using BlacksmithsForge.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlacksmithsForge.Editors
+{
+    public partial class SphereInput : Form
+    {
+        public Sphere Sphere;
+
+        public SphereInput(Sphere sphere)
+        {
+            InitializeComponent();
+            Sphere = sphere;
+        }
+
+        private void greedyCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (greedyCheckBox.Checked) Sphere.Greedy = true;
+            else Sphere.Greedy = null;
+        }
+
+        private void consumesCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (consumesCheckBox.Checked) Sphere.Consumes = true;
+            else Sphere.Consumes = null;
+        }
+
+        private void idTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(idTextBox.Text)) Sphere.ID = idTextBox.Text;
+        }
+
+        private void labelTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(labelTextBox.Text)) Sphere.Label = labelTextBox.Text;
+            else Sphere.Label = null;
+        }
+
+        private void descriptionTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(descriptionTextBox.Text)) Sphere.Description = descriptionTextBox.Text;
+            else Sphere.Description = null;
+        }
+
+        private void actionIdTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(actionIdTextBox.Text)) Sphere.ActionId = actionIdTextBox.Text;
+            else Sphere.ActionId = null;
+        }
+
+        private void fromPathTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(fromPathTextBox.Text)) Sphere.FromPath = fromPathTextBox.Text;
+            else Sphere.FromPath = null;
+        }
+
+        private void enRouteSpherePathTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(enRouteSpherePathTextBox.Text)) Sphere.EnRouteSpherePath = enRouteSpherePathTextBox.Text;
+            else Sphere.EnRouteSpherePath = null;
+        }
+
+        private void windowsSpherePathTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(windowsSpherePathTextBox.Text)) Sphere.WindowsSpherePath = windowsSpherePathTextBox.Text;
+            else Sphere.WindowsSpherePath = null;
+        }
+
+        private void essentialButton_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> essential = Sphere.Essential ?? new();
+            DictionaryEditor dictionaryEditor = new(essential != null ? essential.ToDictionary(entry => entry.Key, entry => (object)entry.Value) : new());
+            if (dictionaryEditor.ShowDialog() == DialogResult.OK)
+            {
+                if (dictionaryEditor.Dictionary.Count > 0) Sphere.Essential = dictionaryEditor.Dictionary.ToDictionary(entry => entry.Key, entry => Convert.ToInt32(entry.Value));
+                else Sphere.Essential = null;
+            }
+        }
+
+        private void requiredButton_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> required = Sphere.Required ?? new();
+            DictionaryEditor dictionaryEditor = new(required != null ? required.ToDictionary(entry => entry.Key, entry => (object)entry.Value) : new());
+            if (dictionaryEditor.ShowDialog() == DialogResult.OK)
+            {
+                if (dictionaryEditor.Dictionary.Count > 0) Sphere.Required = dictionaryEditor.Dictionary.ToDictionary(entry => entry.Key, entry => Convert.ToInt32(entry.Value));
+                else Sphere.Required = null;
+            }
+        }
+
+        private void forbiddenButton_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> forbidden = Sphere.Forbidden ?? new();
+            DictionaryEditor dictionaryEditor = new(forbidden != null ? forbidden.ToDictionary(entry => entry.Key, entry => (object)entry.Value) : new());
+            if (dictionaryEditor.ShowDialog() == DialogResult.OK)
+            {
+                if (dictionaryEditor.Dictionary.Count > 0) Sphere.Forbidden = dictionaryEditor.Dictionary.ToDictionary(entry => entry.Key, entry => Convert.ToInt32(entry.Value));
+                else Sphere.Forbidden = null;
+            }
+        }
+
+        private void ifAspectsPresentButton_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> ifAspectsPresent = Sphere.IfAspectsPresent ?? new();
+            DictionaryEditor dictionaryEditor = new(ifAspectsPresent != null ? ifAspectsPresent.ToDictionary(entry => entry.Key, entry => (object)entry.Value) : new());
+            if (dictionaryEditor.ShowDialog() == DialogResult.OK)
+            {
+                if (dictionaryEditor.Dictionary.Count > 0) Sphere.IfAspectsPresent = dictionaryEditor.Dictionary.ToDictionary(entry => entry.Key, entry => entry.Value.ToString());
+                else Sphere.IfAspectsPresent = null;
+            }
+        }
+
+        private void okayButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
